a446f5430eb852768e2d6e61676f8e758870aae8
malles <matthijs@bixie.nl>
2015-09-30 22:24:20 +0200

    add node

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -35,7 +35,8 @@ class SiteController {
 				'config' => App::module('bixie/formmaker')->publicConfig(),
 				'formitem' => $form,
 				'fields' => array_values($form->fields)
-			]
+			],
+			'node' => App::node()
 		];
 
 	}
