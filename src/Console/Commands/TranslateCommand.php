32b1a2d7596b614261423b3541f206e1c159d69d
malles <matthijs@bixie.nl>
2016-02-29 22:22:15 +0100

    update language

--- src/Console/Commands/TranslateCommand.php
+++ src/Console/Commands/TranslateCommand.php
@@ -1,11 +1,18 @@
 <?php
-
+/**
+ * Create .pot file:
+ *  `php ./pagekit extension:translate bixie/formmaker`
+ * Create .php files
+ *  `php ./pagekit formmaker:translate [en_US]`
+ */
 namespace Bixie\Formmaker\Console\Commands;
 
 use Pagekit\Application\Console\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
 
 class TranslateCommand extends Command
 {
@@ -19,24 +26,24 @@ class TranslateCommand extends Command
 	 */
 	protected $extension = 'formmaker';
 
-    /**
-     * {@inheritdoc}
-     */
-    protected $description = 'Generates extension\'s translation .php file from .pot';
+	/**
+	 * {@inheritdoc}
+	 */
+	protected $description = 'Generates extension\'s translation .php file from .pot';
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function configure()
-    {
-        $this->addArgument('locale', InputArgument::OPTIONAL, 'Locale code (en_US)');
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function configure()
+	{
+		$this->addArgument('locale', InputArgument::OPTIONAL, 'Locale code (en_US)');
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
 		$locale = $this->argument('locale') ?: 'en_US';
 		$rootpath = $this->container->path();
 
@@ -51,6 +58,7 @@ class TranslateCommand extends Command
 		$paths = glob($rootpath . '/packages/bixie/*/languages/'.$locale.'/messages.php', GLOB_NOSORT) ?: [];
 		foreach ($paths as $p) {
 			if ($p != $filename) {
+				$this->line(sprintf("Add %s", $p));
 				$existing = array_merge($existing, include($p));
 			}
 		}
@@ -61,13 +69,13 @@ class TranslateCommand extends Command
 
 		//create translations
 		$translations = [];
+		//get fieldtypes strings
+		$this->getFieldStrings($translations);
 		//insert extra strings
-		foreach (['base', 'fields'] as $extra) {
-			if (file_exists($rootpath . '/packages/bixie/' . $this->extension . '/languages/' . $extra . '.php')) {
-				$extraStrings = include($rootpath . '/packages/bixie/' . $this->extension . '/languages/' . $extra . '.php');
-				foreach ($extraStrings as $string) {
-					$translations[$string] = $locale != 'en_US' && isset($existing[$string])? $existing[$string] : $string;
-				}
+		if (file_exists($rootpath . '/packages/bixie/' . $this->extension . '/languages/base.php')) {
+			$extraStrings = include($rootpath . '/packages/bixie/' . $this->extension . '/languages/base.php');
+			foreach ($extraStrings as $string) {
+				$translations[$string] = $locale != 'en_US' && isset($existing[$string])? $existing[$string] : $string;
 			}
 		}
 		//get pot strings
@@ -80,13 +88,62 @@ class TranslateCommand extends Command
 			if (!$string || isset($system[$string])) continue;
 			$translations[$string] = $locale != 'en_US' && isset($existing[$string])? $existing[$string] : $string;
 		}
-
+		asort($translations);
 		// Write translation file
 		$content  = sprintf('<?php return %s;', var_export($translations, true));
 		file_put_contents($filename, $content);
 
 		$this->line(sprintf("Extension %s: %d strings saved", $this->extension, count($translations)));
 
+	}
+
+	/**
+	 * @param $translations
+	 */
+	protected function getFieldStrings (&$translations) {
+
+		$files = Finder::create()->files();
+		$settingsPath = $this->container->path() . '/packages/bixie/' . $this->extension . '/app/settings';
+		$fieldtypesPath = $this->container->path() . '/packages/bixie/' . $this->extension . '/fieldtypes';
+		if (is_dir($settingsPath)) {
+			$files->in($settingsPath);
+		}
+		if (is_dir($fieldtypesPath)) {
+			$files->in($fieldtypesPath);
+		}
+
+		$fieldfiles = $files->name('*.{vue,js}');
+		if (!$count = count($fieldfiles)) {
+			return;
+		}
+		$this->line("Traversing field files.");
+		$i = 0;
+
+		$progress = new ProgressBar($this->output, $count);
+		$progress->start();
+		foreach ($fieldfiles as $fieldfile) {
+			$content = file_get_contents($fieldfile);
+
+			// field matches (optionlabel|label|title|tip): 'foo',
+			preg_match_all('/\s.(?:label|tip)(\:\s*)(\'|")((?:(?!\2).)+)\2\s*/', $content, $matches);
+			foreach ($matches[3] as $string) {
+				$translations[$string] = $string;
+				$i++;
+			}
+
+			// options matches 'foo': 'value', /*trans*/
+			preg_match_all('/(\'|")((?:(?!\1).)+)\1(\:\s*),*\s*(\'|")((?:(?!\4).)+)\4,?\s*\/\*trans\*\//', $content, $matches);
+			foreach ($matches[2] as $string) {
+				$translations[$string] = $string;
+				$i++;
+			}
+			$progress->advance();
+		}
+
+		$progress->finish();
+		$this->line("\n");
+		$this->line(sprintf("%d strings added from fields", $i));
+		$this->line("\n");
 	}
 
 }
