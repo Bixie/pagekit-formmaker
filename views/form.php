6640d91aa9efbf4b73cdf25f9bf33c7e6011a35b
Matthijs Alles <admin@bixie.nl>
2016-12-01 00:07:16 +0100

    move error, rm close

--- views/form.php
+++ views/form.php
@@ -7,13 +7,8 @@ $view->script('formmaker', 'bixie/formmaker:app/bundle/formmaker.js', ['bixie-fi
 
 	<h1 class="uk-article-title" v-if="!formitem.data.hide_title">{{ formitem.title }}</h1>
 
-	<div class="uk-alert uk-alert-success" v-if="message" data-uk-alert>
-		<a href="" class="uk-alert-close uk-close"></a>
+	<div class="uk-alert uk-alert-success" v-if="message">
 		{{ message | trans }}</div>
-	<div class="uk-alert uk-alert-danger" v-if="error" data-uk-alert>
-		<a href="" class="uk-alert-close uk-close"></a>
-		{{ error | trans }}</div>
-
 
 	<div v-if="!thankyou">
 		<fieldtypes :fields="fields"
@@ -22,7 +17,10 @@ $view->script('formmaker', 'bixie/formmaker:app/bundle/formmaker.js', ['bixie-fi
 
 		<recaptcha v-ref:grecaptcha v-if="formitem.data.recaptcha" :sitekey="config.recaptha_sitekey" :formitem="formitem" class="uk-margin"></recaptcha>
 
-		<div class="uk-margin">
+        <div class="uk-alert uk-alert-danger" v-if="error">
+            {{ error | trans }}</div>
+
+        <div class="uk-margin">
 			<button class="uk-button uk-button-primary" type="submit">{{ formitem.data.submitButton | trans }}</button>
 		</div>
 	</div>
