8c97251330aec08d61ccd2b416a6f81134b5d521
Matthijs Alles <admin@bixie.nl>
2016-12-01 01:08:55 +0100

    add recaptcha strings

--- languages/base.php
+++ languages/base.php
@@ -2,5 +2,9 @@
 return [
 	'Forms' => 'Forms',
 	'Submissions' => 'Submissions',
-	'Submission successful' => 'Submission successful'
+	'Submission successful' => 'Submission successful',
+	'missing-input-response' => 'missing-input-response',
+    'invalid-input-response' => 'invalid-input-response',
+    'invalid-input-secret' => 'invalid-input-secret',
+    'missing-input-secret' => 'missing-input-secret',
 ];
